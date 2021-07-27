4b5edf8193a223e5152dbcb5fb3c11f8c7406bfc
nick <hungryNematode@posteo.net>
2021-07-27 10:30:13 -0500

    Completed more exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  celsius = Number(celsius.toFixed(1));
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius * 9 / 5 + 32;
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
+console.log(ftoc(100));
 module.exports = {
   ftoc,
   ctof
