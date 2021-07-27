899be36f09d9dc7dbe76f898f61816c5c363a3eb
nick <hungryNematode@posteo.net>
2021-07-26 20:45:56 -0500

    completed some exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let string_to_repeat = "";
+            for (let i = 0; i < num; i++) {
+            string_to_repeat += string;
+        };
+        return string_to_repeat;
+    };
 };
 
+console.log(repeatString('hey', -1))
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arrayFromString = string.split("");
+    arrayFromString = arrayFromString.reverse();
+    let reversedString = arrayFromString.join("");
+    return reversedString;
 
 };
+
+console.log(reverseString('Hello'));
 
 module.exports = reverseString;
