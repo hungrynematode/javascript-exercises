e37028659123e9f8d0b0062aab402bc3b668d9d8
nick <hungryNematode@posteo.net>
2021-07-26 22:44:24 -0500

    more completed exercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+    let args = Array.from(arguments);
+    
+    for (i = 1; i < args.length; i++){
+        let index = array.indexOf(args[i])
+        if (index !== -1) {
+            array.splice(index, 1);
+        };
+    };
+    return array;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], "hi" ))
 module.exports = removeFromArray;
